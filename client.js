6ca1b60574e23f0296ba926c411ca426c5b889d0
Owen <253065903@qq.com>
2017-12-13 17:15:00 +0800

    Update client.js

--- client.js
+++ client.js
@@ -1,8 +1,9 @@
 const AipFaceClient = require('baidu-aip-sdk').face;
 
-const APP_ID = '10525733';
-const API_KEY = 'WEsU7l8vdOznwCxnvVTsw4jk';
-const SECRET_KEY = 'o53Wat5NbmwrqFeLfcHRQUYjt4cRvyEX';
+// get from baidu
+const APP_ID = process.env.APP_ID;
+const API_KEY = process.env.API_KEY;
+const SECRET_KEY = process.env.SECRET_KEY;
 
 const client = new AipFaceClient(APP_ID, API_KEY, SECRET_KEY);
 module.exports = client;
